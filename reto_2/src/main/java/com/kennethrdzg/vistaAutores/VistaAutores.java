52b19292ee8a5fd4481e63164ea471c1031a594d
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-05-11 19:35:21 -0600

    Actualizar VistaAutores.java

--- reto_2/src/main/java/com/kennethrdzg/vistaAutores/VistaAutores.java
+++ reto_2/src/main/java/com/kennethrdzg/vistaAutores/VistaAutores.java
@@ -17,8 +17,15 @@ public class VistaAutores{
         for(Autor autor: autores){
             System.out.println("ID: " + autor.getId());
             System.out.println("Nombre: " + autor.getNombre());
+            System.out.println("Link: " + autor.getLink());
             System.out.println("E-mail: " + autor.getEmail());
+            System.out.println("Afiliaciones: " + autor.getAfiliaciones());
             System.out.println("Citado por: " + autor.getCitadoPor());
+            System.out.println("--------------------");
         }
+    }
+
+    public void agregarAutor(Autor autor){
+        autores.add(autor);
     }
 }
